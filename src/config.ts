cd923f1d4630c15b2ab1ae1bea5ee1ba558ea82f
fhowotop <fhowotop@gmail.com>
2025-11-17 17:50:20 +0800

    ``` feat(config): 禁用评论功能并修正 Twikoo 配置
    将 commentConfig 中的 enable 设置为 false，以禁用评论功能。
    同时修正了 twikoo 的 envId 配置，去除了末尾多余的斜杠。
    ```

--- src/config.ts
+++ src/config.ts
@@ -306,9 +306,9 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 };
 
 export const commentConfig: CommentConfig = {
-	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
+	enable: false, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	twikoo: {
-		envId: "http://twikoo.fhowo.top/",
+		envId: "http://twikoo.fhowo.top",
 		lang: "zh-CN", // 设置 Twikoo 评论系统语言为英文
 	},
 };
